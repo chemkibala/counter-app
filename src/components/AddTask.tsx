5271e47c6872dd8eaaf37bfabc84765f20bb088b
Balaguru <chemkibala@gmail.com>
2025-04-11 18:57:11 +0530

    Added the placeholder for "Add Task" input textbox

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -12,6 +12,7 @@ const AddTask = () => {
               value={newTask}
               onChange={(e) => setNewTask(e.target.value)}
               className="border p-1 rounded w-3/4"
+              placeholder="Add the new task here"
             />
     <button onClick={handleAddTask} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded focus:outline-none focus:shadow-outline">Add</button>
     </div>
